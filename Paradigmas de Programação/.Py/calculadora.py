2ec73a762e262f9bd2af2b213bb61c0b7f0ebb9b
Pedro Lucas Teixeira <plslltx@gmail.com>
2025-11-29 23:58:30 -0300

    Add files via upload

---
+++ Paradigmas de Programação/.Py/calculadora.py
@@ -0,0 +1,49 @@
+import operator
+
+operadores = { 
+
+   '+': operator.add,
+   '-': operator.sub,
+   '*': operator.mul,
+   '/': operator.truediv,
+
+}
+
+respar = float(input('Digite um número: '))
+
+
+while True:
+
+    op = input('Digite: [+] somar  [-] subtrair  [*] multiplicar  [/] dividir  [=] finalizar:]')
+
+    if op == "=":
+        print(f'Resultado final: {respar}')
+        break
+
+    if op not in operadores:
+        print('Operador inválido. Tente novamente.')
+        continue
+
+
+    n = float(input('Digite o próximo número: '))
+    respar = operadores[op](respar, n)
+    print(f'Resultado parcial: {respar}')
+
+
+
+
+
+    # Trabalho feito por: Pedro Lucas, Raí e Iago.
+    # Calculadora simples que realiza operações básicas como soma, subtração, multiplicação e divisão.
+    # Explicação do código: Operadores - essa variavel define os calculos que a calculadora pode fazer. 
+    # O while true faz com que o programa continue rodando em loop até o usuario decidir parar o codigo.
+    # O if op == "=" serve para finalizar o programa quando o usuario digitar o sinal de igual.
+    # O Respar significa resultado parcial, ou seja, o resultado que vai sendo atualizado a cada operação feita pelo usuario.
+
+
+#   PPPPP   L
+#   P    P  L
+#   P    P  L
+#   PPPPP   L
+#   P       L
+#   P       LLLLL
